7d59c39bafdff12b8de3fa96fe5a96b6c9a4ac38
julesrb <jbjulesbernard3@gmail.com>
2023-02-26 15:27:08 +0100

    diesntag morgen

--- test.c
+++ test.c
@@ -1,11 +1,50 @@
-#include <stdlib.h>
-#include <mlx.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include "get_next_line/get_next_line.h"
+#include "libft/libft.h"
 
-int main(void)
+#define MAP_SIZE_MAX 100
+
+
+parse_map(char file_name)
 {
-	void	*mlx_ptr;
+	int	fd;
+	int	l;
+	char *line;
+	char *map[MAP_SIZE_MAX];
 
-	mlx_ptr = mlx_init();
-	mlx_destroy_display(mlx_ptr);
-	free(mlx_ptr);
+	fd = open(file_name, O_RDONLY);
+	if (fd < 0)
+		{
+			printf("!! File reading problem !!");
+			return (0);
+		}
+	while (line)
+	{	
+		line = get_next_line(fd);
+		l++
+		map
+	}
+}
+
+int	main (int argc, char argv[])
+{
+	if (argc != 1)
+	{
+		printf("!! Incorrect number of arguments !!");
+		return (0);
+	}
+
+	if (ft_strnstr(argv[1], ".fdf", ft_strlen(argv[1])) == 0)
+	{
+		printf("!! wrong extension !!");
+		return (0);
+	}
+
+	parse_map(argv[1]);
+
+	//save map 
+
+
+	return (0);
 }
