767642afd6bb1c442f757520d95464599e7a8047
Jules <jubernard@4bc0c31.42berlin.de>
2023-03-10 16:40:23 +0100

    first

--- test.c
+++ test.c
@@ -1,42 +1,69 @@
 /* need to inc for open  */
 #include <fcntl.h>
 
+#include "includes/fdf.h"
 #include "libft/includes/libft.h"
 
-#define MAP_SIZE_MAX 100
+int count_split(char **split)
+{
+	int	c;
+
+	c = 0;
+	while (split[c] != 0)
+		c++;
+	return (c);
+}
 
 int parse_map(char *file_name)
 {
-	int	fd;
-	int	row;
-	char *line;
-	char **map;
+	int		fd;
+	int		row;
+	int		col;
+	char	*line;
+	char	**split;
+	t_intersection **map;
+	// map[row][col];
 
 	row = 0;
+	col = 0;
 	fd = open(file_name, O_RDONLY);
 	if (fd < 0)
-		{
-			ft_printf("!! File reading problem !!");
-			return (0);
-		}
+	{
+		ft_printf("!! File reading problem !!");
+		return (0);
+	}
+	line = get_next_line(fd);
+	if (line)
+			row++;
+	split = ft_split(line, ' ');
+	col = count_split(split);
+	ft_printf("\n Map contains %i columns \n",col);
 	while (line)
-	{	
+	{
 		line = get_next_line(fd);
+		//protection
 		if (line)
 			row++;
+		
+		// recheck the free function
+		free(line);
 	}
 	close(fd);
 	ft_printf("\n Map contains %i rows \n",row);
-	map = (char**)malloc(row * sizeof(char*));
+
+	// Re open and copy map. 
+
+	map = malloc(row * col * sizeof(t_intersection));
 	if(!map)
 		return (0);
-	
-		fd = open(file_name, O_RDONLY);
+
+	// NOW I NEED TO DISTRIBUTE THE DATA IN THE @D ARRAY WITH SPLIT
+	fd = open(file_name, O_RDONLY);
 	if (fd < 0)
-		{
-			ft_printf("!! File reading problem !!");
-			return (0);
-		}
+	{
+		ft_printf("!! File reading problem !!");
+		return (0);
+	}
 	while (line)
 	{	
 		line = get_next_line(fd);
@@ -45,12 +72,11 @@ int parse_map(char *file_name)
 	}
 	close(fd);
 
-	ft_split(line, " ");
-	ft_printf("%s",line);
+
 	//itoa
 	//test if color
 	//malloc 3d
-
+	return (0);
 }
 
 int	main (int argc, char *argv[])
